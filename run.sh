c2a6f0a44dd97ae21276a52825a878419b6e8b62
swensonsimanjuntaks5 <81514883+swensonsimanjuntaks5@users.noreply.github.com>
2021-03-28 13:35:02 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,22 @@ sudo adduser swenson sudo
 echo "swenson:123456" | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo apt install nautilus nano -y
+sudo apt install gdebi
+sudo apt -y install firefox
 sudo hostname swenson
-sudo adduser swenson
+sudo adduser swenson chrome-remote-desktop
 sudo apt install tightvncserver
 echo -e "no\n123456\n123456" | tightvncserver :1
 #echo -e "112233\n112233" | su - swenson -c """DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AY0e-g4cfExPsBwTL9HHyQqfRVLHIBpQrkaW96wSh0yJI6WUKNQ46p7rn8dswO6osj-kWg" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)"""
