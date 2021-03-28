7de3b696c23be39db69dace622a1ff0e4b236a91
swensonsimanjuntaks5 <81514883+swensonsimanjuntaks5@users.noreply.github.com>
2021-03-28 14:23:48 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,49 +1,15 @@
-sudo useradd -m swenson
-sudo adduser swenson sudo
-echo "swenson:123456" | sudo chpasswd
-sudo apt-get update
-sudo hostname swenson
-sudo adduser swenson
-sudo apt install tightvncserver
-echo -e "no\n123456\n123456" | tightvncserver :1
-#echo -e "112233\n112233" | su - swenson -c """DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AY0e-g4cfExPsBwTL9HHyQqfRVLHIBpQrkaW96wSh0yJI6WUKNQ46p7rn8dswO6osj-kWg" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)"""
-
-if [[ -z "1iX7KZI6l9uc0cLD5Pj4eStvQnF_7EhMLbe6Y9sFZtD5CNP2g" ]]; then
-  echo "Please set '1iX7KZI6l9uc0cLD5Pj4eStvQnF_7EhMLbe6Y9sFZtD5CNP2g'"
-  exit 2
-fi
-
-if [[ -z "123456" ]]; then
-  echo "Please set '123456' for user: swenson"
-  exit 3
-fi
-
-echo "### Install ngrok ###"
-
-wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
-unzip ngrok-stable-linux-386.zip
-chmod +x ./ngrok
-
-echo "### Update user: swenson password ###"
-echo -e "123456\n123456" | sudo passwd "swenson"
-
-echo "### Start ngrok proxy for 22 port ###"
-
-
-rm -f .ngrok.log
-./ngrok authtoken "1iX7KZI6l9uc0cLD5Pj4eStvQnF_7EhMLbe6Y9sFZtD5CNP2g"
-./ngrok tcp 22 --log ".ngrok.log" &
-
-sleep 10
-HAS_ERRORS=$(grep "command failed" < .ngrok.log)
-
-if [[ -z "$HAS_ERRORS" ]]; then
-  echo ""
-  echo "=========================================="
-  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh swenson@/" | sed "s/:/ -p /")"
-  echo "Connect with remote desktop: https://remotedesktop.google.com/access"
-  echo "=========================================="
-else
-  echo "$HAS_ERRORS"
-  exit 4
-fi
+apt update -y
+apt upgrade -y
+apt install npm -y
+apt-get update
+apt-get upgrade -y
+apt-get install nodejs -y
+apt-get install libwebp-dev -y
+apt-get install mc -y
+apt-get install ffmpeg -y
+apt-get install wget -y
+apt-get install tesseract-ocr -y
+git clone https://swenson12@bitbucket.org/swenson12/botelaina.git
+cd botelaina
+apt install chromium-browser -y
+npm start
